effc72bb4ddcf581f96a5e805ef926cbc0368f40
Ray Benvinda Trajano <121032249+Ray-BT@users.noreply.github.com>
2023-10-23 16:07:28 -0300

    fazendo a aba dos produtos no carrinho

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const menu = document.querySelector('.nav-menu')
 const hamburguer = document.querySelector('.hamburguer')
 const menuclose = document.querySelector('#menuclose')
 
+const cart = document.querySelector('.cart')
+const btnCart = document.querySelector('.btnCart')
+
 
 hamburguer.addEventListener('click', onHamburguerClick)
 menuclose.addEventListener('click', onMenuCloseClick)
+
+btnCart.addEventListener('click', openCart)
 
 function onHamburguerClick() {
     menu.classList.remove('hidden')
@@ -12,4 +17,8 @@ function onHamburguerClick() {
 
 function onMenuCloseClick() {
     menu.classList.add('hidden')
+}
+
+function openCart() {
+    cart.classList.toggle("hidden")
 }
