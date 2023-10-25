a7dc25a9325704ff399f30b1c79eca87895168c3
Ray Benvinda Trajano <121032249+Ray-BT@users.noreply.github.com>
2023-10-25 15:26:07 -0300

    Fazendo as interações da pagina usando javascript

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ const cart = document.querySelector('.cart')
 const btnCart = document.querySelector('.btnCart')
 
 
+const btnPlus = document.querySelector('#btnPlus')
+const btnMinus = document.querySelector('#btnMinus')
+const productCounter = document.querySelector('.counter')
+
+const gallery = document.querySelectorAll('.pic')
+const heroImg = document.querySelector('.product-hero')
+
+const btnNext = document.querySelector('.next')
+const btnPrevious = document.querySelector('.previous')
+
+//Numerical Variables
+let productCounterValue = 1
+
 hamburguer.addEventListener('click', onHamburguerClick)
 menuclose.addEventListener('click', onMenuCloseClick)
 
 btnCart.addEventListener('click', openCart)
+
+btnPlus.addEventListener('click', productCounterPlus)
+btnMinus.addEventListener('click', productCounterMinus)
+
+gallery.forEach(img => {
+    img.addEventListener('click', onThumbClick)
+}) 
+
+btnNext.addEventListener('click', handleBtnClickNext)
+btnPrevious.addEventListener('click', handleBtnClickPrevious)
+
 
 function onHamburguerClick() {
     menu.classList.remove('hidden')
@@ -21,4 +45,64 @@ function onMenuCloseClick() {
 
 function openCart() {
     cart.classList.toggle("hidden")
+}
+
+function productCounterPlus() {
+    setProductCounter(1)
+}
+
+function productCounterMinus() {
+    setProductCounter(-1)
+}
+
+function setProductCounter(value) {
+    if ((productCounterValue + value) > 0) {
+        productCounterValue += value
+        productCounter.innerHTML = productCounterValue
+    }
+
+}
+
+function onThumbClick(event) {
+    //clear active state for all thumbnails
+    gallery.forEach(img => {
+        img.classList.remove('active')
+    })
+    //set active thumbnail
+    event.target.parentElement.classList.add('active')
+    //update hero image
+    heroImg.src = event.target.src.replace('-thumbnail', '')
+}
+
+function handleBtnClickNext() {
+    let imageIndex = getCurrentImageIndex()
+    imageIndex++
+    if (imageIndex > 4) {
+        imageIndex = 1
+    }
+    setHeroImage(imageIndex)
+}
+
+function handleBtnClickPrevious() {
+    let imageIndex = getCurrentImageIndex()
+    imageIndex--
+    if (imageIndex < 1) {
+        imageIndex = 4
+    }
+    setHeroImage(imageIndex)
+}
+
+function getCurrentImageIndex() {
+    const imageIndex = parseInt(heroImg.src.split('\\').pop().split('/').pop().replace('.jpg', '').replace('image-product-', '')) 
+    return imageIndex
+}
+
+function setHeroImage(imageIndex) {
+    heroImg.src = `./images/image-product-${imageIndex}.jpg`
+    //images are not sync
+    gallery.forEach(img => {
+        img.classList.remove('active')
+    })
+    //set active thumbnail
+    gallery[imageIndex-1].classList.add('active')
 }
